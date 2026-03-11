02ec06f9f1bdaaa617dca71827325ccd02c232e8
Arandelle <topaguintsarandell@gmail.com>
2026-03-11 17:40:55 +0800

    feat: screens pages - no design yet

---
+++ src/screens/Orders.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Orders = () => {
+  return (
+    <View>
+      <Text>This is orders</Text>
+    </View>
+  );
+};
+
+export default Orders;
