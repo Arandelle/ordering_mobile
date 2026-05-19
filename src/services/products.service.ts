9998a2f2b7c65ed2075207513bb5846101f10c8a
Arandelle <topaguintsarandell@gmail.com>
2026-05-19 17:40:14 +0800

    rename to products

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,7 +1,7 @@
 import { APP_URL } from '@/constant';
 
 export async function getProducts() {
-  const res = await fetch(`${APP_URL}/products`);
+  const res = await fetch(`${APP_URL}/api/products`);
 
   if (!res.ok) {
     throw new Error('Failed to fetch producst');
