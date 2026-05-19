d74d9159f4d95b996a1d3c5f70a458b1620baddf
Arandelle <topaguintsarandell@gmail.com>
2026-05-19 15:13:15 +0800

    improve (reacy query provider) : add Query Client Provider for tanstack

--- App.tsx
+++ App.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import BottomTabNavigator from "@/navigation/BottomTabNavigator";
 import './global.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <BottomTabNavigator/>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <BottomTabNavigator/>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
