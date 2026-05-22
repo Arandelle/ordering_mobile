9d5d605892b8aa2970618a8c95cc19c1641b9ec8
Arandelle <topaguintsarandell@gmail.com>
2026-05-22 15:53:47 +0800

    Feat: declare cart provider for cart context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,19 @@
 import { Stack } from 'expo-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '../global.css';
+import { CartProvider } from '@/context/CartContext';
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
-        <Stack.Screen name="product/[id]" />
-      </Stack>
+      <CartProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="product/[id]" />
+        </Stack>
+      </CartProvider>
     </QueryClientProvider>
   );
 }
