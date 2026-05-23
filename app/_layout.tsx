41aea72ed11cbcf2df4371a1e46f299f3cf5867f
Arandelle <topaguintsarandell@gmail.com>
2026-05-23 17:11:06 +0800

    feat: add branch provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,18 +2,21 @@ import { Stack } from 'expo-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '../global.css';
 import { CartProvider } from '@/context/CartContext';
+import { BranchProvider } from '@/context/BranchContext';
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
-      <CartProvider>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="(tabs)" />
-          <Stack.Screen name="product/[id]" />
-        </Stack>
-      </CartProvider>
+      <BranchProvider>
+        <CartProvider>
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="(tabs)" />
+            <Stack.Screen name="product/[id]" />
+          </Stack>
+        </CartProvider>
+      </BranchProvider>
     </QueryClientProvider>
   );
 }
