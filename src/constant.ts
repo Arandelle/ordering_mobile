34ee29058dd77ce9eb19f2f9bf0b4b6e5e45c789
Arandelle <topaguintsarandell@gmail.com>
2026-05-26 16:23:42 +0800

    refactor: add food subdomain to aligned with authentication callback url

--- src/constant.ts
+++ src/constant.ts
@@ -1,6 +1,6 @@
 import { Dimensions } from "react-native";
 
-export const APP_URL = "https://www.harrisoninasalbbq.com.ph"
+export const APP_URL = "https://food.harrisoninasalbbq.com.ph"
 
 
 export const SCREEN_HEIGHT = Dimensions.get('screen').height;
