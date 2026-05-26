d342162a35470657efa5d621104d5f81166eb3f6
Arandelle <topaguintsarandell@gmail.com>
2026-05-26 16:24:23 +0800

    feat: declare the auth pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout() {
             <Stack.Screen name="(tabs)" />
             <Stack.Screen name="product/[id]" />
             <Stack.Screen name="checkout" />
+            <Stack.Screen name="auth" />
           </Stack>
         </CartProvider>
       </BranchProvider>
