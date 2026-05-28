78f67b82fa4167a2c5f333a875f529685b7e82b1
Arandelle <topaguintsarandell@gmail.com>
2026-05-28 18:48:33 +0800

    feat: actual peso sign

--- src/screens/Orders/helper/formatMoney.ts
+++ src/screens/Orders/helper/formatMoney.ts
@@ -1,5 +1,5 @@
 export function formatMoney(value: number | undefined) {
-  return `PHP ${(value ?? 0).toLocaleString('en-PH', {
+  return `₱${(value ?? 0).toLocaleString('en-PH', {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   })}`;
