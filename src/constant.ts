c00b091a9cdef9c701e3c248296994ff5db01b02
Arandelle <topaguintsarandell@gmail.com>
2026-09-01 15:03:23 +0800

    feat: use machine's LAN IP for dev

--- src/constant.ts
+++ src/constant.ts
@@ -4,10 +4,8 @@ import { Dimensions, Platform } from "react-native";
 // iOS simulator: use localhost
 // Physical device: use your machine's LAN IP (e.g. 192.168.x.x)
 export const APP_URL = __DEV__
-  ? Platform.OS === 'android'
-    ? 'http://10.0.2.2:3000'
-    : 'http://localhost:3000'
-  : 'https://food.harrisoninasalbbq.com.ph'; // TODO: set production URL
+  ? 'http://192.168.110.215:3000'
+  : 'https://food.harrisoninasalbbq.com.ph';
 
 export const TERMS_OF_USE_URL = `${APP_URL}/policies/terms-of-use`;
 export const PRIVACY_POLICY_URL = `${APP_URL}/policies/privacy-policy`;
